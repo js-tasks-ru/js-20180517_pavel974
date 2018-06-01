4d203b9df07ac08f57cfb7712a757e3e08afaf6f
Pavel Pashynskiy <p_pashynskiy@wargaming.net>
2018-06-01 03:47:58 +0300

    3 module. 2 task

--- 3-module/2-task/index.js
+++ 3-module/2-task/index.js
@@ -4,6 +4,37 @@ let calendar = {
 };
 
 calendar[Symbol.iterator] = function () {
+    let current = new Date( this.from.getFullYear(), this.from.getMonth(), this.from.getDate()).valueOf();
+    let last = new Date( this.to.getFullYear(), this.to.getMonth(), this.to.getDate()).valueOf();
+
+    return {
+        next() {
+
+            if (current !== last) {
+
+                current += 86400000; // 86400000 day in ms
+
+                let thisDate = new Date(current);
+                let result = `${thisDate.getDate()}`;
+
+                if (thisDate.getDate() < 10) {
+                    result = `0${result}`
+                }
+                if (thisDate.getDay() === 0 || thisDate.getDay() === 6) {
+                    result = `[${result}]`;
+                }
+
+                return {
+                    done: false,
+                    value: result
+                };
+
+            } else {
+                return {
+                    done: true
+                }
+            }
+
+        }
+    }
 };
-
-
